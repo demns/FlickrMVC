0d27f344f1561192f1bf60b59fae5c80b99e97e3
Dmitry Samsonau <demn_@tut.by>
2014-09-19 10:11:19 +0300

    Added parameter to check specific user

--- FlickrExample/FlickrExample/Controllers/HomeController.cs
+++ FlickrExample/FlickrExample/Controllers/HomeController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Web.Mvc;
+using System.Web.WebPages;
 using FlickrNet;
 
 namespace FlickrExample.Controllers {
     public class HomeController : Controller {
         // GET: Home
-        public ActionResult Index() {
+        public ActionResult Index(string id) {
+            //example: http://localhost:52495/?id=mark_lj
+            //example: http://localhost:52495/?id=romanulmarius
+            id = id.IsEmpty() ? "127156566@N02" : id;
             var flickr = new Flickr("c2b9248adf2b3a90214a1c20fe1fdef6", "c1c66b0a7f1ca84d");
             flickr.PhotosGetRecent(127156566, 10);
 
             var options = new PhotoSearchOptions {
-                UserId = "127156566@N02",
+                UserId = id,
                 PerPage = 200,
                 Page = 1
             };
